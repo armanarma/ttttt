daddcf197077095238a7bf4e989322450684c52b
armanarmama <armanaskari37@yahoo.com>
2021-09-06 01:47:09 -0700

    hh
    hh

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.rslides').responsiveSlides({
+        speed:500
+    });
+});
+$(document).ready(function(){
+    $("#owl-demo").owlCarousel({
+        autoPlay:true
+    });
+});
+$(document).ready(function(){
+    $(".inline").colorbox({
+        inline:true,
+        width:"450",
+    });
+});
+$(document).ready(function(){
+    $(".sticky-menu").sticky({topSpacing:0});
+});
